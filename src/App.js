0b5dbb4cac6bfbb05811ede516c75ba85dcdbb86
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-05-08 11:02:36 +0600

    login form added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Blogs from './components/Blogs/Blogs';
 import Home from './components/Home/Home/Home';
 import InventoryItemDetail from './components/InventoryItemDetail/InventoryItemDetail';
 import Login from './components/Login/Login/Login';
+import Register from './components/Login/Register/Register';
 import Footer from './components/Shared/Footer/Footer';
 import Header from './components/Shared/Header/Header';
 import NotFound from './components/Shared/NotFound/NotFound';
@@ -22,6 +23,7 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/blog' element={<Blogs></Blogs>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
